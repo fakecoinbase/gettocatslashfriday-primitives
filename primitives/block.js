cbda21409968bbf88165595935a3253868cfa9b1
alex <a@b.ru>
2020-04-15 03:44:01 +0700

    getSize()

--- primitives/block.js
+++ primitives/block.js
@@ -124,7 +124,7 @@ module.exports = (app) => {
             }
 
             let txsize = Math.ceil(this.tx.length.toString(16).length) / 2;//can be %2 != 0, first digit!
-            a += getHeaderBytes().length + 5 + txsize;//bitowl notation - tx: [] 0102TTXX05(SIZEOFLENGTH) type|key(var_str)|value, value is array: value.length|value[0],...,value[n]
+            a += this.getHeaderBytes().length + 5 + txsize;//bitowl notation - tx: [] 0102TTXX05(SIZEOFLENGTH) type|key(var_str)|value, value is array: value.length|value[0],...,value[n]
 
             return a;
         }
